b1faf16e8b9235a5bf447d6d0e549be5226d0436
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-08-03 16:58:09 -0300

    fix: button filters

--- src/pages/Games/index.tsx
+++ src/pages/Games/index.tsx
@@ -1,25 +1,28 @@
-import { Fragment, useEffect } from 'react'
-import Button from '../../components/Button'
+import { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import Filters from '../../components/Filters'
 import Header from '../../components/Header'
 
+import data from '../../assets/games.json'
+
 import { Container, Bar } from './styles'
 
-interface DataProps {
-  item: string
-}
-
-const Games = () => {
-  const customData = require('../../assets/games.json')
-  const games = JSON.parse(customData).types
+const Games: React.FC<{ item: [] }> = () => {
+  const newData = data.map(item => item.types)
 
   return (
     <Fragment>
       <Header />
       <Container>
         <Bar>
-          {customData}
-          <Button route='/new-game' isGreen={true} name='New Bet' />
+          <label>Recent Games</label>
+          <section>
+            <label>Filters</label>
+            {newData.map((item: any) => {
+              return <Filters name={item.type} />
+            })}
+          </section>
+          <Link to='/new-game'>New Bet</Link>
         </Bar>
       </Container>
     </Fragment>
